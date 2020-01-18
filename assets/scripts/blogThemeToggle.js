f1a307e0723f19f6ec3e3d62f5c2222545fa3499
PPanda <ppanda@Anonymous.local>
2020-01-17 20:54:34 -0800

    Many changes (see description)
    - Reorganized files
    - Implemented templating system using JavaScript
    - Added blog article template
    - Light theme toggle on blog pages
    - Scroll to top button on blog pages
    - Altered projects section grid to display cards with same heights
    - Modified CSS
    - Added first blog article
    - Other changes

---
+++ assets/scripts/blogThemeToggle.js
@@ -0,0 +1,1 @@
+$('#dark-toggle').on('click',()=>{if($('#dark-toggle').prop('checked')){$('#light-theme-link').removeAttr('disabled');$('#dark-theme-link').attr('disabled','disabled')}else{$('#light-theme-link').attr('disabled','disabled');$('#dark-theme-link').removeAttr('disabled')}});
